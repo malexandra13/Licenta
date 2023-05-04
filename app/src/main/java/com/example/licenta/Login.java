ea96900d2dfdd62b9155b6f859258bd905da884a
Alexandra <alexandradaniela81@gmail.com>
2023-05-04 13:34:54 +0300

    forgotPassword

--- app/src/main/java/com/example/licenta/Login.java
+++ app/src/main/java/com/example/licenta/Login.java
@@ -2,14 +2,18 @@ package com.example.licenta;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -41,6 +45,7 @@ public class Login extends AppCompatActivity {
     CheckBox checkBoxRememberMe;
     SignInButton googleButton;
     private GoogleSignInClient googleSignInClient;
+    TextView forgotPassword;
 
 
     @Override
@@ -66,6 +71,7 @@ public class Login extends AppCompatActivity {
         buttonLogin = findViewById(R.id.loginButton);
         progressBar = findViewById(R.id.progressBar);
         textViewRegister = findViewById(R.id.registerNow);
+        forgotPassword = findViewById(R.id.forgotPassword);
         googleButton = findViewById(R.id.googleButton);
 
         //!TO-DO : implement check box "remember me"
@@ -87,6 +93,48 @@ public class Login extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), Register.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
+                View dialogView = getLayoutInflater().inflate(R.layout.dialog_forgotpassword, null);
+                EditText emailBox = dialogView.findViewById(R.id.emailBox);
+                builder.setView(dialogView);
+                AlertDialog dialog = builder.create();
+                dialogView.findViewById(R.id.btnReset).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String userEmail = emailBox.getText().toString();
+                        if (TextUtils.isEmpty(userEmail) && !Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()){
+                            Toast.makeText(Login.this, "Enter your registered email id", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        mAuth.sendPasswordResetEmail(userEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(Login.this, "Check your email", Toast.LENGTH_SHORT).show();
+                                    dialog.dismiss();
+                                } else {
+                                    Toast.makeText(Login.this, "Unable to send, failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                });
+                dialogView.findViewById(R.id.btnCancel).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+                if (dialog.getWindow() != null){
+                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+                }
+                dialog.show();
             }
         });
 
